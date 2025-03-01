bce1e1bf7f1770ed3b60f306b0c8b590a5521176
root <root@HAL.>
2025-03-01 20:56:39 +0100

    Moved to linux

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use gtk::prelude::*;
+use gtk::{glib, Application, ApplicationWindow,Button};
+
+const APP_ID: &str = "org.gtk_rs.HelloWorld2";
+
+fn main() -> glib::ExitCode {
+    // Create a new application
+    let app = Application::builder().application_id(APP_ID).build();
+
+    // Connect to "activate" signal of `app`
+    app.connect_activate(build_ui);
+
+    // Run the application
+    app.run()
+}
+
+fn build_ui(app: &Application) {
+    // Create a window and set the title
+    let button = Button::builder()
+        .label("press!")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+
+    button.connect_clicked(|button| {
+        // Set the label to "Hello World!" after the button has been clicked on
+        button.set_label("Hello World!");
+    });
+
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("My GTK App")
+        .child(&button)
+        .build();
+
+    // Present window
+    window.present();
+}
